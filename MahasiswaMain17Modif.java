d6d98b3df652ae1b999362e706a861c72cb042bb
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-02-19 22:08:45 +0700

    Jobsheet 2 Algoritma Struktur Data

---
+++ MahasiswaMain17Modif.java
@@ -0,0 +1,15 @@
+public class MahasiswaMain17Modif {
+    public static void main(String[] args) {
+        Mahasiswa17Modif mhs1 = new Mahasiswa17Modif();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(5.60);
+        mhs1.tampilkanInformasi();
+
+    }
+}
